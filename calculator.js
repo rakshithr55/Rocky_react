1c61ecddf331d5b477e3b60cfbea6c8b5dd5a369
rakshithr55 <120468407+rakshithr55@users.noreply.github.com>
2023-02-26 11:27:47 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,13 @@
+function clearScreen() {
+        document.getElementById("res").value = "";
+    }
+     
+function disp(value) {
+        document.getElementById("res").value += value;
+    }
+     
+function calculate() {
+        var a = document.getElementById("res").value;
+        var b = eval(a);
+        document.getElementById("res").value = b;
+    }
